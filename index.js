dd487892433e33369e317bf65301a63abd009393
jess <jessica.elessa@gmail.com>
2022-04-06 13:51:58 +0200

    add a .gitignore with node_modules

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-//--------- EXPRESS AND HANDLEBARS ------------//
+//--------- EXPRESS AND MANGOOSE ------------//
 const express = require("express");
 const app = express();
 const port = 8000;
-const handlebars = require("express-handlebars");
-//-------------- OTHER LIBRARIES --------------//
-const path = require("path");
+const mongoose = require("mongoose");
+//---------- AUTHENTIFICATION ----------------//
 const cookieParser = require("cookie-parser");
+const { MONGODB_URI } = process.env;
+
+//-------------- OTHER LIBRARIES --------------//
+const handlebars = require("express-handlebars");
+const path = require("path");
 //------------ HANDLEBARS CONFIG --------------//
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 
 //---------------- MIDDLEWARES -----------------//
+app.use(express.json());
 app.use(cookieParser());
 // * For CSS and JS in HTML
 app.use(express.static(path.join(__dirname, "/public")));
@@ -32,20 +37,21 @@ app.get("/signup", async (_req, res) => {
 // ! Create a user
 app.post("/signup", (req, res) => {
   console.log(req.body);
-  // Create a token
+  // ! Create a token
   const token = "eyJabcdteslojm";
   res.cookie("jwt", token);
   res.redirect("/profile");
 });
 
+// * LOGIN
 app.get("/login", async (_req, res) => {
   res.render("login");
 });
 
 app.post("/login", (req, res) => {
-  // Create a user
+  // ! Create a user
   console.log(req.body);
-  // Create a token
+  // ! Create a token
   const token = "eyJabcdteslojm";
   res.cookie("jwt", token);
   res.redirect("/products");
